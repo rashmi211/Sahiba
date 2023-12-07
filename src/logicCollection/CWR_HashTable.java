d0f2cc1dc228737df8710a4735f6dc67c7dab96b
rashmi211 <salunkers213@gmail.com>
2023-12-07 21:36:50 +0530

    Added hashtable code

--- src/logicCollection/CWR_HashTable.java
+++ src/logicCollection/CWR_HashTable.java
@@ -2,6 +2,7 @@ package logicCollection;
 
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 public class CWR_HashTable {
 	public static void main(String[] args) {
@@ -14,10 +15,18 @@ public class CWR_HashTable {
 		hm.put("Rani", 15);
 		hm.put("Kartik", 20);
 		hm.put("Varun", 13);
-		hm.put(null, null);
+		//hm.put(null, null);
 		
 		System.out.println(hm);
 		
-	}
-
+		System.out.println(hm.containsKey("Rani"));
+		
+		
+		System.out.println(hm.replace("Varun", 13, 19));
+		System.out.println(hm);
+		
+		for(Map.Entry<String,Integer>e: hm.entrySet()) {
+		      System.out.println(e.getKey());
+		      System.out.println(e.getValue());
+		}}
 }
